523b499c7a32172f281420591fdeb1e84a26c10e
MartinSkachkov <81876862+MartinSkachkov@users.noreply.github.com>
2021-08-03 16:51:37 +0300

    prototype of the board game scrabble
    $ ./scrabble
    Player 1: COMPUTER
    Player 2: science
    Player 1 wins!

---
+++ pset2/Scrabble.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word)
+{
+    int sumPoints = 0;
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (isalpha(word[i]))
+        {
+            int position = toupper(word[i]) - 'A'; // 'E' e 69 ASCII - 'A' e 65 = 4
+            sumPoints += POINTS[position];
+        }
+    }
+    return sumPoints;
+}
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+    
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+    
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie\n");
+    }
+    
+}
